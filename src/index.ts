534e6328f94c93551e13d5ddc6f62b87020a02e1
antoinelutsen <antoine.lutsen@gmail.com>
2026-03-19 13:42:59 +0100

    swagger

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import express from 'express';
 import pokemonCardRouter from './pokemonCards/pokemonCard.routes'
 import userRouter from "./users/user.routes"
+import swaggerUi from "swagger-ui-express"
+import swaggerSpec from "./swagger"
 
 export const app = express();
 const port = process.env.PORT || 3000;
@@ -8,6 +10,7 @@ const port = process.env.PORT || 3000;
 app.use(express.json());
 app.use('/pokemons-cards', pokemonCardRouter)
 app.use("/users", userRouter)
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec))
 
 export const server = app.listen(port);
 
---
+++ src/swagger.ts
@@ -0,0 +1,22 @@
+import swaggerJsdoc from "swagger-jsdoc"
+
+const options: swaggerJsdoc.Options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Pokemon API",
+      version: "1.0.0",
+      description: "API de gestion de cartes Pokémon",
+    },
+    servers: [
+      {
+        url: "http://localhost:3000",
+      },
+    ],
+  },
+  apis: ["./src/routes/*.ts"], 
+}
+
+const swaggerSpec = swaggerJsdoc(options)
+
+export default swaggerSpec
